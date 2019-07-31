e63600acad6c2e22b666bcc2f93d19a0ec0a0e17
Nanditap <nandupat@googlemail.com>
2019-07-31 20:45:44 +0100

    Stage 1 : :Normal Price

---
+++ ShoppingCart/ShoppingCart/Fruits.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart
+{
+    class Fruits
+    {
+        
+
+        public string Fname { get; set; }
+        public double Fprice { get; set; }
+
+       
+
+        public Fruits(string fname)
+        {
+            Fname = fname.Trim();
+            if (Fname.ToUpper() == "APPLE")
+            {
+                Fprice = 0.60d;
+            }
+            else if (Fname.ToUpper() == "ORANGE")
+            {
+                Fprice = 0.25d;
+            }
+            else if (Fname.ToUpper() == "EXIT")
+            {
+                Console.WriteLine("Thank you for shopping with us :)");
+            }
+            else
+            {
+                Console.WriteLine("We only sale Apple or Orange ");
+            }
+        }
+
+
+    }
+}
---
+++ ShoppingCart/ShoppingCart/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ShoppingCart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("********************************************************");
+            Console.WriteLine("* Fruits Shopping Cart                                 *");
+            Console.WriteLine("*                                                      *");
+            Console.WriteLine("*                                                      *");
+            Console.WriteLine("* One Apple =  0.60pence                               *");
+            Console.WriteLine("* One Orange = 0.25pence                               *");
+            Console.WriteLine("*                                                      *");
+            Console.WriteLine("* You can enter your fruit choice                      *");
+            Console.WriteLine("* until you have entered exit                          *");
+            Console.WriteLine("*                                                      *");
+            Console.WriteLine("* Payment owed will be displayed                       *");
+            Console.WriteLine("* after exit                                           *");
+            Console.WriteLine("*                                                      *");
+            Console.WriteLine("*********************************\n\n");
+            Basket bsk = new Basket();
+            while (true)
+            {
+                Console.WriteLine("* Input your Choice: Apple/Orange/Exit? ");
+                string input = Console.ReadLine();
+                bsk.AddBasket(input);
+                
+                if (input.Trim() == "exit")
+                {
+                    break;
+                }
+
+
+            }
+
+            Console.WriteLine("* \nYou need to pay  £{0:0.00}" , bsk.calcBasketTotal());
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
